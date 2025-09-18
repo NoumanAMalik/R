65ff17d1887dd5e0fce4d0cb7d9ff7c546358bd7
Nouman Malik <noumancodes@gmail.com>
2025-09-18 15:42:49 +0000

    Ready to R

---
+++ hello_world.R
@@ -0,0 +1,15 @@
+# Simple R test script
+print("Hello, World! R is working correctly.")
+
+# Test basic arithmetic
+result <- 2 + 3
+print(paste("2 + 3 =", result))
+
+# Test vector creation
+vec <- c(1, 2, 3, 4, 5)
+print("Vector:")
+print(vec)
+
+# Test mean calculation
+mean_val <- mean(vec)
+print(paste("Mean of vector:", mean_val))
